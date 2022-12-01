128e959e3dbf220c211ec35bd5a28a28b18c323a
Matthew Kokotovich <mkokotovich@gmail.com>
2022-12-01 15:57:02 -0600

    bug

--- 2022/1/find_max_cals.py
+++ 2022/1/find_max_cals.py
@@ -11,6 +11,10 @@ def find_max_cals():
                 current_total = 0
             else:
                 current_total += int(line.strip())
+        # the last elf may not have a new line
+        if current_total > max_total:
+            max_total = current_total
+
 
     print(f"Max: {max_total}")
 
--- 2022/1/find_top_three.py
+++ 2022/1/find_top_three.py
@@ -10,6 +10,10 @@ def find_top_three():
                 current_total = 0
             else:
                 current_total += int(line.strip())
+        # the last elf may not have a new line
+        if current_total:
+            all_cals.append(current_total)
+            all_cals = sorted(all_cals, reverse=True)[:3]
 
     print(f"Top 3: {all_cals}")
     print(f"Sum: {sum(all_cals)}")
